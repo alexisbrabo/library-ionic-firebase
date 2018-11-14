d84b0c2550adebc1673f1d07d58a350fad359d63
alexisbrabo <alexis_brabo@hotmail.com>
2018-11-14 09:14:36 -0300

    build android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_QJa4QXRpx4trJFaXDhCNNyXiKCCVYnc',
+    authDomain: 'biblioteca-857fd.firebaseapp.com',
+    databaseURL: 'https://biblioteca-857fd.firebaseio.com',
+    projectId: 'biblioteca-857fd',
+    storageBucket: 'biblioteca-857fd.appspot.com',
+    messagingSenderId: '831547463246'
+  }
 };
