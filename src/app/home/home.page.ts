ef01985b24069f2f0d08e7865978cb3f660a400d
alexisbrabo <alexis_brabo@hotmail.com>
2018-11-14 13:50:13 -0300

    Implement search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,16 +8,32 @@ import { Book, BookService } from '../services/book.service';
 })
 export class HomePage implements OnInit {
   books: Book[];
+  booksLoaded: Book[];
+  booksRef: Book[];
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
     this.bookService.getBooks().subscribe(response => {
       this.books = response;
+      this.booksLoaded = response;
     });
   }
 
   remove(book) {
     this.bookService.removeBook(book.id);
+  }
+
+  filterBooks(ev: any) {
+    this.getAll();
+
+    const val = ev.target.value;
+    this.books = this.booksLoaded.filter(function (item) {
+      return !val && val.trim() === '' ? item.name : item.name.toLowerCase().includes(val.toLowerCase());
+    });
   }
 }
